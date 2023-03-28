3c609678d971c90af2515fc753bf7a2d029f8aa0
GrishaRubitel <post.germa@gmail.com>
2023-03-28 16:49:37 +0300

    Fork synced

--- TSLUMDEncoder.py
+++ TSLUMDEncoder.py
@@ -1,33 +1,24 @@
-from enum import Enum
-from typing import List
-
 from MessagersInterfaces import Listener, Notifier, T
-from RossServer import *
-from UMDDecoder import *
+from RossServer import RossEvent
+from UMDDecoder import TSLEvent
+
 
 class RossEventToTSLUMD(Listener[RossEvent], Notifier[bytes]):
     def __init__(self, listener: Listener[bytes]):
-        self._a = bytes()
-        self._a = []
         self._listener = listener
 
     def on_message(self, message: RossEvent, notifier: Notifier[RossEvent]):
         """
-        Param:
-        Returns:
+        with TSLEvent modul turns RossEvent message to bytes
+        Param: RossEvent
+        Returns: bytes
         """
         mes_state = message.get_camera_state()
-        b = bytes
-        if mes_state == 0:
-            list_of_tallies = [False, False, False, False]
-        elif mes_state == 1:
+        list_of_tallies = [False] * 4
+        if mes_state == 1:
             list_of_tallies = [True, False, False, False]
         elif mes_state == 2:
             list_of_tallies = [False, True, False, False]
         elif mes_state == 3:
             list_of_tallies = [True, True, False, False]
-        b = TSLEvent(message.get_camera_id(), list_of_tallies, 1.0)
-        self._a.append(b)
-        for i in range (0, 16):
-            self._a.append(0)
-        self._listener(self._a, self)
+        self._listener(TSLEvent(message.get_camera_id(), list_of_tallies, 1.0).to_bytes(), self)
